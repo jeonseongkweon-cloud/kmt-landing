76a168663004f2d0a0a4884492dd03dec65aa3ea
jeonseongkweon-cloud <jeonseongkweon@gmail.com>
2026-08-30 09:26:10 +0900

    Add files via upload

--- class/config.js
+++ class/config.js
@@ -1,7 +1,7 @@
 window.KMT_CLASS_CONFIG = Object.freeze({
   appName: "계명태권도 CLASS SYSTEM",
-  version: "0.9.0",
-  stage: "WORK 9차 · Android 문자 발신기",
+  version: "1.0.0",
+  stage: "WORK 10차 · GLOBAL SPARK 진입",
   homeUrl: "../",
   classPeriods: ["1부", "2부", "3부", "4부", "기타"]
 });
